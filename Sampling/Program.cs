73b219f287aaba58e6b24e566eebae7bf8997625
Mohamed Fazrin <mfmfazrin1986@gmail.com>
2023-01-09 05:58:14 +0300

    Initial commit

---
+++ Sampling/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+
+using static System.Console;
+
+using static Sampling.RequestNumberHelper;
+
+WriteLine("Printing random strings!");
+
+var sw = Stopwatch.StartNew();
+
+for (int i = 0; i < 1000; i++)
+{
+  WriteLine(RandomPassword());
+}
+
+sw.Stop();
+
+WriteLine($"Elapsed: {sw.ElapsedTicks} ticks");
+
+//Average Time taken per random password 
+
+WriteLine( $"Average Time taken per random password: {sw.ElapsedTicks / 1000} ticks");
+
+ReadLine();
+
+/*
+var student1 = new Student
+{
+  Name = "Jon"
+};
+
+var student2 = new Student
+{
+  Name = "Jon"
+};
+
+WriteLine(student1.Equals(student2) ? "Both objects are equal" : "Both objects are not equal");
+
+public class Student
+{
+  public string Name { get; set; }
+}
+*/
---
+++ Sampling/RequestNumberHelper.cs
@@ -0,0 +1,8 @@
+﻿using Bogus;
+
+namespace Sampling;
+
+public static class RequestNumberHelper
+{
+    public static string RandomPassword() => new Faker().Random.AlphaNumeric(11).ToUpper();
+}
